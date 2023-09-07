3331062bb2ac8a78832f8d9fc66190c66fb21e8f
CGU-B1029048 <B1029048cgu@gmail.com>
2023-09-07 17:12:33 +0800

    test2

--- Lab01/lab01.asm
+++ Lab01/lab01.asm
@@ -1,16 +1,18 @@
+ORG OH
+
 mov R0, #20h; //R0 = A[start]
 mov R1, #28h; //B = B[start]
 mov R2, #8; //R2 = N
 mov R3, #0;
 loop:
-mov A,@R0;
-mov B,@R1;
-mul AB;
-add A, R3;
-mov R3, A;
-inc R0;
-inc R1;
-djnz R2, loop;
+    mov A,@R0;
+    mov B,@R1;
+    mul AB;
+    add A, R3;
+    mov R3, A;
+    inc R0;
+    inc R1;
+    djnz R2, loop;
 here:
-sjmp here;
+    sjmp here;
 END
