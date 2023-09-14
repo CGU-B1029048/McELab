5be18e80a0159d21cfe25c7ebe9b8355ece94fce
Chien <100185381+b1029009Chien@users.noreply.github.com>
2023-09-14 16:59:43 +0800

    Update lab02_example.asm

--- Lab02/lab02_example.asm
+++ Lab02/lab02_example.asm
@@ -22,28 +22,43 @@ LEGACY_PAGE  equ  00h
     mov P2MDOUT, #0ffh
     mov SFRPAGE, #LEGACY_PAGE
 
-    ;resert 0 for P1 and P2
-    mov P1, #0
-    mov P2, #0
-;P1.6 pressed P2 rotate left 1s pulse
+		;detect and set button, restart P1 and P2
+		mov P1, #0
+		mov P2, #0
+
+;P1.7 pressed,
 MAIN:
-    mov A, P1.7
-    JNZ Loop_L
-    mov A, P1.6
-    JNZ Loop_R
-    
-    mov A, P1
-    JZ MAIN
-Loop_L:
-    MOV P2, A
-    LCALL Delay
-    RL A
-    LJMP Loop_L
+		MOV A, P1
+		JZ MAIN
+
+    JB P1.7, Loop_R
+
+		JB P1.6, Loop_L
+
+		JB P1.5, Loop_odd
+
+		JB P1.4, Loop_even
+
 Loop_R:
     MOV P2, A
     LCALL Delay
     RR A
     LJMP Loop_R
+
+Loop_L:
+    MOV P2, A
+    LCALL Delay
+    RL A
+    LJMP Loop_L
+
+Loop_odd:
+		MOV P2, A
+    LCALL Delay
+    RL A
+    LJMP Loop_L
+
+
+
 Delay: MOV R0, #50
 Delay0: MOV R1, #40
 Delay1: MOV R2, #249
@@ -51,5 +66,4 @@ Delay2: DJNZ R2, Delay2
         DJNZ R1, Delay1
         DJNZ R0, Delay0
         RET
-
 END
