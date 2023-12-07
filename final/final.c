7552429d2f4bec9899a482d4f12e1ed34a8fa7a2
Chien <100185381+b1029009Chien@users.noreply.github.com>
2023-12-07 15:43:21 +0800

    Update final.c

--- final/final.c
+++ final/final.c
@@ -1,8 +1,10 @@
 #include "C8051F040.h"
 #include "glcd.h"
-#include <stdio.h>
+//#include <stdio.h>
 #include <stdlib.h>
 int mode, z_cur, y_cur;
+//int food_x, food_y;
+//int goldfood_x = 1, goldfood_y = 1;
 int status = 1;//right
 /*******************************************************************************
  *
@@ -367,41 +369,83 @@ void GLCD_Clean() {
  *
  ******************************************************************************/
 
-const unsigned char pacman_right[2][16] = 
-{{0x00, 0x00, 0xE0, 0xF8, 0xF8, 0xFC, 0xFE, 0xFE, 0xFE, 0xFE, 0x66, 0x34, 0x1C, 0x08, 0x00, 0x00},
-{0x00, 0x00, 0x07, 0x1F, 0x1F, 0x3F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7E, 0x3C, 0x38, 0x10, 0x00, 0x00}};
+const unsigned char pacman_right[8] = {0x00,0x38, 0x7c, 0xfe, 0xee, 0xc6, 0x82, 0x00};
 
-const unsigned char pacman_down[2][16] = 
-{{0x00, 0x80, 0xE0, 0xF0, 0xF8, 0xF8, 0xFC, 0xFC, 0xFC, 0xFC, 0xF8, 0xF8, 0xF0, 0xE0, 0x80, 0x00},
-{0x00, 0x03, 0x1F, 0x3F, 0x1F, 0x0F, 0x07, 0x03, 0x03, 0x07, 0x0F, 0x13, 0x31, 0x1F, 0x03, 0x00}};
+const unsigned char pacman_down[8] = {0x00, 0x78, 0x3c, 0x1e, 0x1e, 0x3c, 0x78, 0x00};
 
-const unsigned char pacman_up[2][16] = 
+/*const unsigned char pacman_up[2][16] = 
 {{0x00, 0xC0, 0xF8, 0xFC, 0xF8, 0xF0, 0xE0, 0xC0, 0xC0, 0xE0, 0xF0, 0xC8, 0x8C, 0xF8, 0xC0, 0x00},
-{0x00, 0x01, 0x07, 0x0F, 0x1F, 0x1F, 0x3F, 0x3F, 0x3F, 0x3F, 0x3F, 0x1F, 0x0F, 0x07, 0x01, 0x00}};
+{0x00, 0x01, 0x07, 0x0F, 0x1F, 0x1F, 0x3F, 0x3F, 0x3F, 0x3F, 0x3F, 0x1F, 0x0F, 0x07, 0x01, 0x00}};*/
+
+/*
+const unsigned char food[2] = {0xC0, 0xC0};
+const unsigned char gold_food[4] = {0x60, 0xFF, 0xFF, 0x60};
+
+
+void generatefood(int foodmode){
+  food_x = rand() % 64;
+  food_y = rand() % (128 - foodmode*2);
+}
+
+void drawfood(int foodmode){
+	int i, x, y;
+	x = (foodmode > 1)? goldfood_x: food_x;
+	y = (foodmode > 1)? goldfood_y: food_y;
+	Set_DisplayStartLine (x);
+
+	Set_DisplayOn ((mode = (y > 64))? 1 : 0); 
+	Set_Yaddr (y);	
+	
+	for (i=0;i< (y < 64 && y > 64 - foodmode*2)? 64-y: foodmode*2;i++){
+		Send_Data (food[i]);
+	}
+
+	if(y < 64 && y > 64 - foodmode*2){
+		Set_DisplayOn (1);
+		for(i = 1; i < y + foodmode*2 -64; i++)
+			Send_Data (i);
+	}
+}*/
+
+
+
 
 void draw(int mode, int z_in, int y_in, int status) //mode 0 right, 1 left
 {
-	const unsigned char (*pacman)[16];
-	int i, x;
+	unsigned char* pacman;
+	int i;
 	Set_DisplayStartLine (z_in);
-	if(status == 1) pacman = pacman_right;
-	if(status == 3) pacman = pacman_up;
-	if(status == 4) pacman = pacman_down;
-	for (x = 0; x < 2; x++) {
-		Set_Xaddr (x);
+	if(status == 1 || 2) pacman = pacman_right;
+	if(status == 3 || 4) pacman = pacman_down;
+		Set_Xaddr (1);
 
 		if (mode && y_in < 64) { //draw left
 			Set_Yaddr (y_in);
-			for (i=0;i < ((y_in > 48) ? 64 - y_in : 16);i++)
-				(status == 2)? Send_Data (pacman[x][15-i]):Send_Data (pacman[x][i]);
+			for (i=0;i < ((y_in > 56) ? 64 - y_in : 8);i++)
+				if (status == 2)
+					Send_Data (pacman[7-i]);
+				else if (status == 3){
+					if(pacman[i] != 0)
+					 	Send_Data (pacman[((i+3)%6)? (i+3)%6: 6]);
+					else 
+						Send_Data (pacman[i]);
+				} else 
+					Send_Data (pacman[i]);
 		}
 
 		if (!mode && y_in > 48) { //draw right
 			Set_Yaddr((y_in < 64)? 0: y_in-64);
-			for (i = ((y_in > 63) ? 0 : 64 - y_in);i < 16;i++)
-				(status == 2)? Send_Data (pacman[x][15-i]):Send_Data (pacman[x][i]);
+			for (i = ((y_in > 63) ? 0 : 64 - y_in);i < 8;i++)
+				if (status == 2)
+					Send_Data (pacman[7-i]);
+				else if (status == 3){
+					if(pacman[i] != 0)
+						Send_Data (pacman[((i+3)%6)? (i+3)%6: 6]);
+					else 
+						Send_Data (pacman[i]);
+				} else 
+					Send_Data (pacman[i]);
 		}
-	}
 }
 	
 
@@ -440,6 +484,9 @@ int main ()
 		} else if(P1 == 8 && y_cur >0){
 			y_cur--;
 			status = 2;//left
+		} else if(P1 == 16){
+			//generatefood(1);
+			//drawfood(1);
 		}
 		//draw right
 		mode = 0;
