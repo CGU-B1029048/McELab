0c7abafeb7049981382288b243449a79ceb07238
Chien <100185381+b1029009Chien@users.noreply.github.com>
2023-12-07 18:36:29 +0800

    Update final.c

--- final/final.c
+++ final/final.c
@@ -1,34 +1,28 @@
 #include "C8051F040.h"
 #include "glcd.h"
-//#include <stdio.h>
+//#include "LCD.h"
 #include <stdlib.h>
 int mode, z_cur, y_cur;
 int food_x, food_y;
 int goldfood_x = 1, goldfood_y = 1;
-int status = 1;//right
+int pac_status;//right
 /*******************************************************************************
  *
  * functions for configuring the hardware
  *
  ******************************************************************************/
 
-void
-set_GLCD_WriteMode ()
-{
+void set_GLCD_WriteMode (){
 	P4MDOUT = 0xff;
 }//end of function set_GLCD_WriteMode
 
 
-void
-set_GLCD_ReadMode ()
-{
+void set_GLCD_ReadMode (){
 	P4MDOUT = 0x00;
 	P4 = 0xff;
 }//end of function set_GLCD_ReadMode
 
-void
-system_init_config ()
-{
+void system_init_config (){
 	//turn-off the watch-dog timer
 	WDTCN = 0xde;
 	WDTCN = 0xad;
@@ -51,16 +45,12 @@ system_init_config ()
  *
  ******************************************************************************/
 
-void
-GLCD_delay ()
-{
+void GLCD_delay (){
 	int i;
-	for (i=0;i<10;i++);
+	for (i=0;i<200;i++);
 }//end of function GLCD_delay
 
-void
-GLCD_Write (char P2_cword, char P4_cword)
-{
+void GLCD_Write (char P2_cword, char P4_cword){
 	char P2_cword_rep;
 
 	P2_cword_rep = P2_cword;
@@ -83,9 +73,7 @@ GLCD_Write (char P2_cword, char P4_cword)
 	
 }//end of function GLCD_Write
 
-char
-GLCD_Read (char P2_cword)
-{
+char GLCD_Read (char P2_cword){
 	char status;
 	char P2_cword_rep;
 
@@ -118,9 +106,7 @@ GLCD_Read (char P2_cword)
  *
  ******************************************************************************/
 
-char
-GLCD_ReadStatus ()
-{
+char GLCD_ReadStatus (){
 	char P2_cword;
 	char status;
 
@@ -136,9 +122,7 @@ GLCD_ReadStatus ()
 }//end of function GLCD_ReadStatus
 
 
-char
-GLCD_ReadData ()
-{
+char GLCD_ReadData (){
 	char P2_cword;
 	char dat;
 
@@ -153,9 +137,7 @@ GLCD_ReadData ()
 	return dat;
 }//end of function GLCD_ReadData
 
-int
-GLCD_IsBusy ()
-{
+int GLCD_IsBusy (){
 	char status;
 
 	status = GLCD_ReadStatus ();
@@ -179,16 +161,12 @@ GLCD_IsReset ()
 }//end of function GLCD_IsReset
 
 
-int
-GLCD_IsON ()
-{
+int GLCD_IsON (){
 	return !GLCD_IsOFF ();
 }//end of function GLCD_IsON
 
 
-int
-GLCD_IsOFF ()
-{
+int GLCD_IsOFF (){
 	char status;
 
 	status = GLCD_ReadStatus ();
@@ -205,9 +183,7 @@ GLCD_IsOFF ()
  *
  ******************************************************************************/
 
-void
-Set_Xaddr (char x)
-{
+void Set_Xaddr (char x){
 	char P2_cword, P4_cword;
 
 	///prepare control words
@@ -226,9 +202,7 @@ Set_Xaddr (char x)
 }//end of function Set_Xaddr
 
 
-void
-Set_Yaddr (char y)
-{
+void Set_Yaddr (char y){
 	char P2_cword, P4_cword;
 
 	///prepare control words
@@ -247,9 +221,7 @@ Set_Yaddr (char y)
 }//end of function Set_Yaddr
 
 
-void
-Set_DisplayStartLine (char z)
-{
+void Set_DisplayStartLine (char z){
 	char P2_cword, P4_cword;
 
 	///prepare control words
@@ -286,9 +258,7 @@ Send_Data (char pattern)
 	GLCD_Write (P2_cword, P4_cword);
 }//end of function Send_Data
 
-void
-Set_DisplayOn (int mode)
-{
+void Set_DisplayOn (int mode){
 	char P2_cword, P4_cword;
 
 	///prepare control words
@@ -364,32 +334,23 @@ void GLCD_Clean() {
 	}
 }
 /*******************************************************************************
- *
  * Drawing functions that you implement
- *
  ******************************************************************************/
 
 const unsigned char pacman_right[8] = {0x00,0x38, 0x7c, 0xfe, 0xee, 0xc6, 0x82, 0x00};
-
-const unsigned char pacman_down[8] = {0x00, 0x78, 0x3c, 0x1e, 0x1e, 0x3c, 0x78, 0x00};
-
-/*const unsigned char pacman_up[2][16] = 
-{{0x00, 0xC0, 0xF8, 0xFC, 0xF8, 0xF0, 0xE0, 0xC0, 0xC0, 0xE0, 0xF0, 0xC8, 0x8C, 0xF8, 0xC0, 0x00},
-{0x00, 0x01, 0x07, 0x0F, 0x1F, 0x1F, 0x3F, 0x3F, 0x3F, 0x3F, 0x3F, 0x1F, 0x0F, 0x07, 0x01, 0x00}};*/
-
-
+const unsigned char pacman_up[8] = {0x00, 0x78, 0x3c, 0x1e, 0x1e, 0x3c, 0x78, 0x00};
 //const unsigned char food[2] = {0xC0, 0xC0};
 const unsigned char gold_food[4] = {0x60, 0xFF, 0xFF, 0x60};
 
 
 void generatefood(int foodmode){
   food_x = rand() % 64;
-  if (food_x % 8 == 7) food_x - 1;
+  if (food_x % 8 == 7 || food_x % 8 == 0) food_x - 1;
   food_y = rand() % (128 - foodmode*2);
 }
 
 void drawfood(){
-	int x, y;
+	int i, x, y;
 	int food = 0x03;
 	x = food_x, y = food_y;
 	
@@ -401,7 +362,7 @@ void drawfood(){
 	Set_Yaddr (y);	
 	
 	// determine which level on current x page
-	for (int i = 0; i <= x%8; i++) {
+	for (i = 0; i <= x%8; i++) {
 		food *= 2;
 	}
 	Send_Data(food);
@@ -417,23 +378,21 @@ void drawfood(){
 }
 
 
-
-
-void draw(int mode, int z_in, int y_in, int status) //mode 0 right, 1 left
-{
-	unsigned char* pacman;
+void draw(int mode, int z_in, int y_in) {
+	//mode 0 right, 1 left
+	unsigned char *pacman;
 	int i;
 	Set_DisplayStartLine (z_in);
-	if(status == 1 || 2) pacman = pacman_right;
-	if(status == 3 || 4) pacman = pacman_down;
+	if(pac_status == 1 || pac_status == 2) pacman = pacman_right;
+	if(pac_status == 3 || pac_status == 4) pacman = pacman_up;
 		Set_Xaddr (0);
 
 		if (mode && y_in < 64) { //draw left
 			Set_Yaddr (y_in);
 			for (i=0;i < ((y_in > 56) ? 64 - y_in : 8);i++)
-				if (status == 2)
+				if (pac_status == 2)
 					Send_Data (pacman[7-i]);
-				else if (status == 3){
+				else if (pac_status == 4){
 					if(pacman[i] != 0)
 					 	Send_Data (pacman[((i+3)%6)? (i+3)%6: 6]);
 					else 
@@ -445,9 +404,9 @@ void draw(int mode, int z_in, int y_in, int status) //mode 0 right, 1 left
 		if (!mode && y_in > 48) { //draw right
 			Set_Yaddr((y_in < 64)? 0: y_in-64);
 			for (i = ((y_in > 63) ? 0 : 64 - y_in);i < 8;i++)
-				if (status == 2)
+				if (pac_status == 2)
 					Send_Data (pacman[7-i]);
-				else if (status == 3){
+				else if (pac_status == 4){
 					if(pacman[i] != 0)
 						Send_Data (pacman[((i+3)%6)? (i+3)%6: 6]);
 					else 
@@ -456,8 +415,14 @@ void draw(int mode, int z_in, int y_in, int status) //mode 0 right, 1 left
 					Send_Data (pacman[i]);
 		}
 }
-	
 
+void move_pacman(int pac_status) {
+	if(pac_status == 1 && y_cur < 120) y_cur++;
+	else if (pac_status == 2 && y_cur > 0) y_cur--;
+	else if (pac_status == 3 && z_cur > 8) z_cur--;
+	else if (pac_status == 4 && z_cur < 63) z_cur++;
+	GLCD_delay();
+}
 
 /*******************************************************************************
  *
@@ -476,36 +441,40 @@ int main ()
 	GLCD_Clean();
 
 	P1 = 0x00;
-	z_cur = 0;
+	z_cur = 32;
 	y_cur = 48;
-	status = 1;
+	pac_status = 4;
 	
 	while(1){
 		if(P1 == 1){//up
-			z_cur++;
-			status = 3;
+			//z_cur++;
+			pac_status = 4;
 		} else if(P1 == 2){//down
-			z_cur--;
-			status = 4;
-		} else if(P1 == 4 && y_cur < 96){
-			y_cur++;
-			status = 1;//right
-		} else if(P1 == 8 && y_cur >0){
-			y_cur--;
-			status = 2;//left
+			//z_cur--;
+			pac_status = 3;
+		} else if(P1 == 4){
+			//y_cur++;
+			pac_status = 1;//right
+		} else if(P1 == 8){
+			//y_cur--;
+			pac_status = 2;//left
 		} else if(P1 == 16){
-			//generatefood(1);
-			//drawfood(1);
+			generatefood(1);
+			drawfood();
 		}
+		move_pacman(pac_status);
 		//draw right
 		mode = 0;
 		Set_DisplayOn (mode);
-		draw(mode, z_cur, y_cur,status);	
+		draw(mode, z_cur, y_cur);	
 
 		//draw left
 		mode = 1;
 		Set_DisplayOn (mode);
-		draw(mode, z_cur, y_cur,status);
+		draw(mode, z_cur, y_cur);
+
+		//GLCD_delay();
+
 	}
 }//end of function main
 
