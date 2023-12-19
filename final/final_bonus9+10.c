ef59199881c75ce2412926122f449ae38eeb754d
Chien <100185381+b1029009Chien@users.noreply.github.com>
2023-12-19 16:10:13 +0800

    Update final_bonus9+10.c

--- final/final_bonus9+10.c
+++ final/final_bonus9+10.c
@@ -1,120 +1,11 @@
 #include "C8051F040.h"
 #include "glcd.h"
-#include "LCD.h"
 #include <stdlib.h>
 int mode, x_cur, y_cur;
 int old_x, old_y, old_ghost_x, old_ghost_y;
 int ghost_x, ghost_y;
 int goldfood_x, goldfood_y;
 int pac_status, speed;
-/*******************************************************************************
- *
- * LCD
- *
- ******************************************************************************/
-char LCD_status;
-void LCD_PortConfig (){
-	//initialize SFR setup page
-	SFRPAGE = CONFIG_PAGE;                 // Switch to configuration page
-	//setup the cross-bar and configure the I/O ports
-	XBR2 = 0xc0;
-	P3MDOUT = 0x3f;
-	
-	//set to normal mode
-	SFRPAGE = LEGACY_PAGE;
-}//end of function LCD_PortConfig ()
-
-/****************
-The delay_lcd seem to be not long enough. 
-Try to change the default value as 10000...
-****************/
-unsigned int delay_lcd=20000;
-
-void LCD_Delay (){
-	int i;
-	for (i=0;i<delay_lcd;i++); // wait for a long enough time...
-}
-
-void LCD_SendCommand (char cmd);
-
-void LCD_Init (){
-	LCD_SendCommand(0x02);        // Initialize as 4-bit mode
-	LCD_SendCommand (0x28);		//Display function: 2 rows for 4-bit data, small 
-	LCD_SendCommand (0x0e);		//display and curson ON, curson blink off
-	LCD_SendCommand (0x01);		//clear display, cursor to home
-	//LCD_SendCommand (0x10);		//cursor shift left
-	//LCD_SendCommand (0x06);		//cursor increment, shift off
-}
-
-void LCD_Status_SetRS (){
-	LCD_status = LCD_status | 1;
-}
-
-void
-LCD_Status_ClearRS ()
-{
-	LCD_status = LCD_status & 0xfe;
-}
-
-void LCD_Status_SetWord (char word){
-	word = word & 0x0f;
-	LCD_status = LCD_status & 0x03;
-	LCD_status = LCD_status | (word<<2);
-}
-
-void LCD_Status_SetEnable (){
-	LCD_status = LCD_status | 0x02;
-}
-
-
-void LCD_Status_ClearEnable (){
-	LCD_status = LCD_status & 0xfd;
-}
-
-
-void LCD_SendCommand (char cmd){
-	LCD_Status_ClearRS ();
-
-	///send the higher half
-	LCD_Status_SetWord ((cmd>>4) & 0x0f);
-	LCD_Status_SetEnable ();
-	P3 = LCD_status;
-	LCD_Delay ();
-	LCD_Status_ClearEnable ();
-	P3 = LCD_status;
-	LCD_Delay ();
-
-	///send the lower half
-	LCD_Status_SetWord (cmd&0x0f);
-	LCD_Status_SetEnable ();
-	P3 = LCD_status;
-	LCD_Delay ();
-	LCD_Status_ClearEnable ();
-	P3 = LCD_status;
-	LCD_Delay ();
-}
-
-void LCD_SendData (char dat){
-	LCD_Status_SetRS ();
-
-	///send the higher half
-	LCD_Status_SetWord ((dat>>4) & 0x0f);
-	LCD_Status_SetEnable ();
-	P3 = LCD_status;
-	LCD_Delay ();
-	LCD_Status_ClearEnable ();
-	P3 = LCD_status;
-	LCD_Delay ();
-
-	///send the lower half
-	LCD_Status_SetWord (dat&0x0f);
-	LCD_Status_SetEnable ();
-	P3 = LCD_status;
-	LCD_Delay ();
-	LCD_Status_ClearEnable ();
-	P3 = LCD_status;
-	LCD_Delay ();
-}
 
 /*******************************************************************************
  *
@@ -564,20 +455,6 @@ int main (){
 		}
 		GLCD_delay(speed);
 	}
-	// Game over
-	P3 = 0xff;
-	LCD_PortConfig ();
-	LCD_Init();
-	LCD_SendCommand(0x01);
-	LCD_SendData('G');
-	LCD_SendData('A');
-	LCD_SendData('M');
-	LCD_SendData('E');
-	LCD_SendData(' ');
-	LCD_SendData('O');
-	LCD_SendData('V');
-	LCD_SendData('E');
-	LCD_SendData('R');
 	while(1) P6 =~P6;
 	return 0;
 }//end of function main
